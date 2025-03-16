e82f1c15c0c1f1469f522e2b2dfc1299e926f8bb
Urmatbek Dubanaev <urmatnd@gmail.com>
2025-03-16 22:20:14 +0600

    multiplication from 1 to 10

--- src/Urmat.java
+++ src/Urmat.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Urmat {
     public static void main(String[] args){
         // Тескери тартип (for цикл)
         for (int i = 10; i >= 1 ; i--) {
             System.out.println("Тескери: " + i);
         }
+
+        // Коптук таблица
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Кобойтуу таблица: 1ден 10го чейин каалаган сан жазыныз.");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(number * i);
         }
+
+    }
 }
