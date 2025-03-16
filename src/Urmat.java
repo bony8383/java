98a7358ca762ba335bdb5f1e7e9b7b14267713fe
Urmatbek Dubanaev <urmatnd@gmail.com>
2025-03-16 20:45:10 +0600

    countdown from 10 to 1

--- src/Urmat.java
+++ src/Urmat.java
@@ -1,2 +1,8 @@
 public class Urmat {
+    public static void main(String[] args){
+        // Тескери тартип (for цикл)
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.println("Тескери: " + i);
+        }
+        }
 }
