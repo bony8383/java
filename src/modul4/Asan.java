cff3c08f0d951b98201b392be518c4c92d8e834b
Asan-Mamadov <mamadovasan660@gmail.com>
2025-03-16 06:03:14 +0100

    asan homework

---
+++ src/modul4/Asan.java
@@ -0,0 +1,12 @@
+package modul4;
+
+public class Asan {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("плюс: " + i);
+        }
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("минус" + i);
+        }
+    }
+}
