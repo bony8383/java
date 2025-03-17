4200d82568d42cbe02bce4951d7c8d8ab87a19d0
ZhamalTynch97 <zhamal.020997@mail.de>
2025-03-17 15:53:40 +0100

    Array Beispiele

---
+++ src/arrayMisal/arrayMisal.java
@@ -0,0 +1,15 @@
+package arrayMisal;
+
+ public class arrayMisal {
+    public static void main( String [] args){
+            int[] numbers ;
+            numbers = new int[ 8];
+            numbers = new int [ ]{ 10, 20, 30 , 40 ,50};
+            System.out.println( " birinchi element : " + numbers[ 0]);
+            System .out .println( " ekinchi element : " + numbers [1]);
+
+
+        }
+    }
+
+}
