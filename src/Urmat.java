cb01d9ec6150e8a1164081e0db73c4c540853db8
Urmatbek Dubanaev <urmatnd@gmail.com>
2025-03-17 14:44:09 +0600

    ask numbers

--- src/Urmat.java
+++ src/Urmat.java
@@ -15,5 +15,17 @@ public class Urmat {
             System.out.println(number + " x " + i + " = " + (number * i));
         }
 
+        // Сан 0 эмес учурда сурай беруу
+        System.out.println("Сан жазыныз: ");
+        int input = scanner.nextInt();
+
+        while (input != 0) {
+            System.out.print("Сан жазыныз (чыгуу учун 0 саны): ");
+            input = scanner.nextInt();
+            System.out.println("Сиз жаздыныз: " + input);
+        }
+
+        System.out.println("Программа аяктады.");
+
     }
 }
