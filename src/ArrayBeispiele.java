d1d9fb75ab99c81fa6e1b47c165f91421d249c30
ZhamalTynch97 <zhamal.020997@mail.de>
2025-03-17 15:34:50 +0100

    Array Beispiele

---
+++ src/ArrayBeispiele.java
@@ -0,0 +1,11 @@
+public class ArrayBeispiele {
+    public static void main( String [] args){
+        int[] numbers ;
+        numbers = new int[ 8];
+        numbers = new int [ ]{ 10, 20, 30 , 40 ,50};
+        System.out.println( " birinchi element : " + numbers[ 0]);
+        System .out .println( " ekinchi element : " + numbers [1]);
+
+
+    }
+}
