ad4f73617551df6b6c8837e1674cd32b21148670
ZhamalTynch97 <zhamal.020997@mail.de>
2025-03-17 12:38:52 +0100

    for

---
+++ src/Zhamal.java
@@ -0,0 +1,8 @@
+public class Zhamal {
+    public static void main(String[] args) {
+        for ( int i = 22 ;   i >= 1;  i --  ) {
+            System.out.println( " Artka sanoo  - " + i );
+        }
+    }
+}
+
