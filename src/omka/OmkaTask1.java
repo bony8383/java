ecaef994b494d58259a3c3180f6a7d97a29aaf9e
John Doe <omurbek.aksaibek@gmail.com>
2025-03-17 20:23:52 +0100

    Modul4 (for-do while)

---
+++ src/omka/OmkaTask1.java
@@ -0,0 +1,9 @@
+package omka;
+
+public class OmkaTask1 {
+    public static void main(String[] args) {
+        for (int i = 10; i >=1; i--) {
+            System.out.println("Tetiri: " + i);
+        }
+    }
+}
