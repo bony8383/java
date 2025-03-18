0374663f6a4237ae340ca092d6f73519411fc404
Aelita <aelita.aibekova@icloud.com>
2025-03-18 16:56:29 +0100

    Homework1

---
+++ src/aelita/Task.java
@@ -0,0 +1,9 @@
+package aelita;
+
+public class Task {
+    public static void main(String[] args){
+        for(int i = 10; i >= 1; i--){
+            System.out.println("San tetiri " + i);
+        }
+    }
+}
