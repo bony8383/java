f1d908f6c78143b4ffe167677d7bedbe423fc74d
Aelita <aelita.aibekova@icloud.com>
2025-03-18 17:47:43 +0100

    Delete src/aelita/Task.java

--- src/aelita/Task.java
+++
@@ -1,9 +0,0 @@
-package aelita;
-
-public class Task {
-    public static void main(String[] args){
-        for(int i = 10; i >= 1; i--){
-            System.out.println("San tetiri " + i);
-        }
-    }
-}
