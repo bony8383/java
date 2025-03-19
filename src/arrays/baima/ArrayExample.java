9af335b3e5ebbdc5bc918846c6d2ce3545fedce0
Baima <baima.omorbekova.ch@gmail.com>
2025-03-19 18:09:50 +0600

    Create array example file

--- src/arrays/baima/ArrayExample.java
+++ src/arrays/baima/ArrayExample.java
@@ -1,4 +1,9 @@
 package arrays.baima;
 
 public class ArrayExample {
+    public static void main (String[] args) {
+        for (int i=1; i<=5; i++){
+            System.out.println("Сан: "+i);
+        }
+    }
 }
