fd79eaac6c8f47160c3b9efe3ce2c16403636dd9
ZhamalTynch97 <zhamal.020997@mail.de>
2025-03-19 11:29:29 +0100

    if jana else

---
+++ src/ifelse/zhamal/IfElse.java
@@ -0,0 +1,23 @@
+package ifelse.zhamal;
+
+public class IfElse {
+    public static void main(String[] args) {
+        System.out.println("Salam , Javanyn jashoochulary");
+
+        int temperatur  = 38;
+        int temperatur2 = 36 ;
+
+        if ( temperatur >= temperatur2) {
+            System.out.println(" Patient Hr. M hat Fieber , er soll ein Fiebersenkende Infusion kriegen ");
+        }
+        else  {
+            System .out .println( " Patient hat normales Temperatur , deswegen benötigt er keine Medikamnte ");
+        }
+
+
+
+
+
+    }
+
+}
