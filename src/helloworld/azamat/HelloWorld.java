0c1cc4e991dd5d4e3b4378b0523fe03a9d3638b4
John Doe <omurbek.aksaibek@gmail.com>
2025-03-23 21:05:10 +0100

    Hello_world

--- src/helloworld/azamat/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package helloworld.azamat;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        String name = "Azamat";
-        System.out.println("Salam, " + name);
-    }
-}
