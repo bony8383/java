12292bb27a88e852b5b6a6ff8538d0271ba6fece
Urmatbek Dubanaev <urmatnd@gmail.com>
2025-03-24 14:53:32 +0600

    one-dimensional arrays

--- src/arrays/urmat/OneArray.java
+++ src/arrays/urmat/OneArray.java
@@ -17,5 +17,23 @@ public class OneArray {
         System.out.println("Массив: " + Arrays.toString(numbers));
         System.out.println("Массив суммасы: " + Arrays.stream(numbers).sum());
         System.out.println("Массив орточо мааниси: " + Arrays.stream(numbers).average());
+
+        //Максимум табуу
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++){
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Максималдуу маани: " + max);
+
+        //Минимум табуу
+        int min  = numbers[0];
+        for (int i = 1; i < numbers.length; i++){
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        System.out.println("Минималдуу маани: " + min);
     }
 }
