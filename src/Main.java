7d405ed4bc2fc3c86a2099367718e30560fa4710
Kasymbekov1 <askar.kasymbekov1@gmail.com>
2025-03-27 19:35:58 +0100

    7.1 #64

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import oop.askar.Car;
+
+     public class Main {
+            public static void main(String[] args) {
+                Car myCar = new Car("Toyota", 60);
+                myCar.showInfo(); // Автомобиль: Toyota, Скорость: 60 км/ч
+                myCar.accelerate(20); // Toyota ускоряется. Теперь скорость: 80 км/ч
+            }
     }
-}
