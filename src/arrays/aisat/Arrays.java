7e89d709532921c3d48f11d34ee5870f4d09befa
Atabekoba-Aizat <aizatatabekova17@gmail.com>
2025-04-06 17:43:09 +0200

    Arrays

---
+++ src/arrays/aisat/Arrays.java
@@ -0,0 +1,11 @@
+package arrays.aisat;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "banana", "orange", "pineapple"};
+
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+    }
+}
