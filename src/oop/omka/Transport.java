668b33524f67c3ee07b81ce5b5f420ea05acb653
John Doe <omurbek.aksaibek@gmail.com>
2025-04-14 15:10:50 +0200

    OOP_Murastoo_Getter§Setter

---
+++ src/oop/omka/Transport.java
@@ -0,0 +1,12 @@
+package oop.omka;
+
+public class Transport {
+    public void start() {
+        System.out.println("Унаа иштетилди.");
+    }
+
+    public void stop() {
+        System.out.println("Унаа токтотулду.");
+    }
+}
+// Jany
---
+++ src/oop/omka/Vw.java
@@ -0,0 +1,28 @@
+// Version 1.0
+package oop.omka;
+
+public class Vw {
+    public static void main(String[] args){
+        Auto car1 = new Auto();
+        car1.setMarka("Toyota");
+        car1.setPreis(5000);
+
+        Auto car2 = new Auto("BMW", 10000);
+
+        // Наследованный методдор
+        car1.start();
+        car1.all();
+        car1.stop();
+
+        System.out.println();
+
+        car2.start();
+        car2.all();
+        car2.stop();
+
+        // Setter текшерүү
+        car1.setPreis(7000);
+        System.out.println("Жаңыртылган баасы: " + car1.getPreis() + " доллар");
+    }
+}
+// Jany
