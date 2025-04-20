f2995802e5bb907cf875a5a4f5c7945aa0638513
bony8383 <88745088+bony8383@users.noreply.github.com>
2025-04-20 22:23:21 +0200

    JAVA2B-22: HelloWorld классы кошулду

--- src/helloworld/Meerim/HelloWorld.java
+++ src/helloworld/Meerim/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld.Meerim;
 public class HelloWorld {
     public static void main(String[] args) {
         String s = "Salam Kyrgyzstan!";
-        System.out.println(s);
+        System.out.println( s );
 
     }
 }
