785468db7010337fec42dd84bffca651d0566a27
bony8383 <88745088+bony8383@users.noreply.github.com>
2025-04-20 20:41:26 +0200

    first commit

---
+++ src/helloworld/Meerim/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld.Meerim;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s = "Salam Kyrgyzstan!";
+        System.out.println(s);
+
+    }
+}
